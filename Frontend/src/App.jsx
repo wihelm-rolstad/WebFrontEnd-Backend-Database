646910663616608527464b7ad2deb5e0a976d4d5
Wilhelm Svensgaard Rolstad <wilhelsr@stud.ntnu.no>
2026-01-30 20:41:12 +0100

    enhancement(login): created login and views between register and login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
+import Register from './components/register.jsx'
 import Login from './components/login.jsx'
 import './App.css'
 
 function App() {
+  
+  const [active, setActive] = useState("a");
+
   return (
     <>
-      <Login/>
+      {active === "a" && (
+        <div>
+          <Register/>
+          <p style={{cursor: "pointer"}} onClick={() => setActive("b")}>Already have an account? Log in.</p>
+        </div>
+      )}
+
+      {active === "b" && (
+        <div>
+          <Login/>
+          <p style={{cursor: "pointer"}} onClick={() => setActive("a")}>Don't have an account? Register.</p>
+        </div>
+      )}
     </>
   )
 }
