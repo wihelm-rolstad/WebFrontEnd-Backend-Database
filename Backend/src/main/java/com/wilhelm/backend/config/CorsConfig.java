2771ad7722c0d671a35496aedee2b5b16989a529
Wilhelm Svensgaard Rolstad <wilhelsr@stud.ntnu.no>
2026-02-11 12:07:53 +0100

    added domain to cors config

--- Backend/src/main/java/com/wilhelm/backend/config/CorsConfig.java
+++ Backend/src/main/java/com/wilhelm/backend/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig implements WebMvcConfigurer {
             .allowedOrigins(
                 "http://localhost:5174",
                 "http://127.0.0.1:5174",
-                "https://fitness-app-amber-xi-29.vercel.app"
+                "https://fitness-app-amber-xi-29.vercel.app",
+                "https://liftlog.no"
             )
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*");
